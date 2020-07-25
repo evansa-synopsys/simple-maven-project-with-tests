2bc673fa02e222d5b18bd304a4abcba8587d974a
Alex Evans <evansa@synopsys.com>
2020-07-24 18:10:33 -0600

    adfdfadsf

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,9 +31,10 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        if (r > 0.1) {
+        int s = (int) r;
+        if (s > 0.1) {
             fail("oops");
-        } else if (r < 0.2) {
+        } else if (s < 0.2) {
             throw new AssumptionViolatedException("skipping");
         }
     }
